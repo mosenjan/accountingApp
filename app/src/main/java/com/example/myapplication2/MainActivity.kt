aba465f7f28969f0fd2a649d8fae321069e7837a
mosenjan <as.mohsen2@gmail.com>
2021-03-18 14:50:29 +0330

    first commit

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.myapplication2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var toolbar: ActionBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigation)
+
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+
+        when (it.itemId) {
+            R.id.navigation_exit -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_home -> {
+                toolbar.title = "میزکار"
+               // val homeFragment = HomeFragment.newInstance()
+               // openFragment(homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_setting -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_profile -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+}
