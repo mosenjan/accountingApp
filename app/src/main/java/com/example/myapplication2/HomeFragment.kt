9a2199bebfc41281182d53c5bb39e20c41ad83a1
mosenjan <as.mohsen2@gmail.com>
2021-03-27 22:12:09 +0430

    office night commit

--- app/src/main/java/com/example/myapplication2/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication2/HomeFragment.kt
@@ -5,22 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -28,10 +16,7 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -51,7 +36,7 @@ class HomeFragment : Fragment() {
 
     fun onClick(){
         materialButton3.setOnClickListener{
-           // findNavController().navigate(HomeFragmentDirections.actionHomepageFragmentToWriteArticleFragment())
+           // findNavController().navigate(HomeFragmentDirection.actionHomepageFragmentToWriteArticleFragment())
         }
     }
 }
